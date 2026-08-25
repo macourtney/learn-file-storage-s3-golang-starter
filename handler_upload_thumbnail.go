90eac3195a2347c859e9d25c9fb8693e11ce5823
Matthew Courtney <matt-git@port87.com>
2026-08-25 17:39:27 -0400

    Store thumbnails as base64 data URLs in database

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 	"mime"
@@ -77,12 +78,8 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	videoThumbnails[videoID] = thumbnail{
-		data:      imageData,
-		mediaType: mediaType,
-	}
-
-	thumbURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
+	imageString := base64.StdEncoding.EncodeToString(imageData)
+	thumbURL := fmt.Sprintf("data:%s;base64,%s", mediaType, imageString)
 	video.ThumbnailURL = &thumbURL
 
 	err = cfg.db.UpdateVideo(video)
